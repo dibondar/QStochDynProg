ee31eb08a771f9762c1444f429881d0f1775f990
dbondar <kdfstudio@gmail.com>
2017-10-06 20:53:57 -0400

    improvements

--- monte_carlo_tree_search.py
+++ monte_carlo_tree_search.py
@@ -12,7 +12,10 @@ class MCTreeSearch(object):
     get_control = itemgetter('control')
     get_iteration = itemgetter('iteration')
 
-    def __init__(self, init_state, init_control, propagator, control_switching, cost_func):
+    # for visualization
+    get_original_weight = itemgetter('original_weight')
+
+    def __init__(self, init_state, init_control, propagator, control_switching, cost_func, nsteps=10):
         """
         Constructor
         :param init_state: (object) The initial state of a system
@@ -26,12 +29,14 @@ class MCTreeSearch(object):
         :param control_switching: A graph specifying the rules of switching of controls.
 
         :param cost_func: An objective function to be maximized.
-        """
 
+        :param nsteps: number of steps to take during the simulation stage (aka, the horizon length)
+        """
         # Save parameters
         self.propagator = propagator
         self.control_switching = control_switching
         self.cost_func = cost_func
+        self.nsteps = nsteps
 
         self.init_state = init_state
         self.init_control = init_control
@@ -46,6 +51,7 @@ class MCTreeSearch(object):
         self.decision_graph.add_node(
             0,
             weight=self.cost_func(init_state),
+            original_weight=self.cost_func(init_state),
             control=init_control,
             iteration=0,
         )
@@ -79,17 +85,20 @@ class MCTreeSearch(object):
                 # the name of a new node
                 new_node = len(self.decision_graph)
 
-                # add the new node and edge
+                ##########################################################################
+                #
+                #   Simulation stage to get the weight of a new node
+                #
+                ##########################################################################
+                new_weight = self.simulation(C, state)
+
+                # add the new node and the edge
                 self.decision_graph.add_node(
                     new_node,
                     control=C,
                     iteration=current_iteration,
-                    ##########################################################################
-                    #
-                    #   Simulation stage to get the weight of a new node
-                    #
-                    ##########################################################################
-                    weight=self.simulation(C, state),
+                    weight=new_weight,
+                    original_weight=new_weight,
                 )
                 self.decision_graph.add_edge(new_node, leaf_node)
 
@@ -101,22 +110,30 @@ class MCTreeSearch(object):
             #
             ##########################################################################
 
+            # backpropagate starting from each new node added in the expansion stage
             for new_node in new_nodes:
+
                 new_weight = self.get_weight(self.decision_graph.node[new_node])
                 n = new_node
 
-                while len(self.decision_graph[n]):
-                    assert len(self.decision_graph[n]) == 1
-                    # go one level up
-                    n, = self.decision_graph[n]
-                    if self.get_weight(self.decision_graph.node[n]) >= new_weight:
-                        break
-                    else:
-                        # updade the value
-                        self.decision_graph.node[n].update(weight=new_weight)
+                try:
+                    # loop until len(self.decision_graph[n]) == 0
+                    while True:
+                        # go one level up
+                        n, = self.decision_graph[n]
 
+                        # extract property of the node
+                        prop = self.decision_graph.node[n]
 
-
+                        # decide whether to keep backpropagating
+                        if self.get_weight(prop) >= new_weight:
+                            break
+                        else:
+                            # update the value of the weight
+                            prop.update(weight=new_weight)
+                except ValueError:
+                    # the root node was reached
+                    pass
 
     def selection(self):
         """
@@ -135,39 +152,43 @@ class MCTreeSearch(object):
         # start from the root and select successive child nodes down to a leaf node
         current_node = 0
 
-        while len(self.decision_graph[current_node]):
+        try:
+            # loop until len(self.decision_graph[current_node]) == 0
+            while True:
+                # extract where to go next
+                candidate_nodes = self.decision_graph[current_node]
 
-            # extract where to go next
-            candidate_nodes = self.decision_graph[current_node]
+                # extract probabilities from weights
+                p = np.fromiter(
+                    (self.get_weight(self.decision_graph.node[_]) for _ in candidate_nodes),
+                    np.float,
+                    len(candidate_nodes)
+                )
+                p /= p.sum()
 
-            # extract probabilities from weights
-            p = np.fromiter(
-                (self.get_weight(self.decision_graph.node[_]) for _ in candidate_nodes),
-                np.float,
-                len(candidate_nodes)
-            )
-            p /= p.sum()
+                # make a random choice
+                current_node = np.random.choice(candidate_nodes.keys(), p=p)
 
-            # make a random choice
-            current_node = np.random.choice(candidate_nodes.keys(), p=p)
+                # extract the control that brings to the selected node
+                control = self.get_control(self.decision_graph.node[current_node])
 
-            # extract the control that brings to the selected node
-            control = self.get_control(self.decision_graph.node[current_node])
+                # propagate to the current node
+                state = self.propagator(control, state)
 
-            # propagate to the current node
-            state = self.propagator(control, state)
+        except ValueError:
+            # a leaf node is reached
+            pass
 
         # restore the original directionality of the graph
         self.decision_graph.reverse(copy=False)
 
         return current_node, control, state
 
-    def simulation(self, control, state, nsteps=10):
+    def simulation(self, control, state):
         """
         Perform the simulation stage of the Monte Carlo tree search
         :param control: initial control
         :param state: initial state
-        :param nsteps: numer of random steps to take
         :return: maximum value of cost function
         """
         # make a local copy of the state
@@ -175,7 +196,7 @@ class MCTreeSearch(object):
 
         max_cost = -np.inf
 
-        for _ in range(nsteps):
+        for _ in range(self.nsteps):
             # update state
             state = self.propagator(control, state)
 
@@ -197,7 +218,7 @@ class MCTreeSearch(object):
         Decision graph plotting utility.
         :return:
         """
-        return [self.get_weight(n) for n in self.decision_graph.node.values()]
+        return [self.get_original_weight(n) for n in self.decision_graph.node.values()]
 
     def get_edge_color(self):
         """
@@ -212,7 +233,7 @@ class MCTreeSearch(object):
         :return:
         """
         return {
-            node:(self.get_iteration(prop), self.get_weight(prop))
+            node:(self.get_iteration(prop), self.get_original_weight(prop))
             for node, prop in self.decision_graph.node.items()
         }
 
@@ -244,12 +265,13 @@ if __name__=='__main__':
         field[field.size / 2],
         CPropagator(field_switching),
         field_switching,
-        CCostFunc()
+        CCostFunc(),
+        nsteps=100,
     )
 
-    for _ in range(5):
+    for _ in range(1):
 
-        mcts.make_rounds(10)
+        mcts.make_rounds(100)
 
         plt.title("Landscape")
         plt.xlabel("time variable (dt)")
@@ -260,7 +282,7 @@ if __name__=='__main__':
             node_color=mcts.get_node_color(),
             edge_color=mcts.get_edge_color(),
             arrows=False,
-            alpha=0.6,
+            alpha=0.8,
             node_shape='s',
             with_labels=False,
             linewidths=0,
