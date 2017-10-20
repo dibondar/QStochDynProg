70f08705dbe15f548f5b783f0bc804043e1c4559
dbondar <kdfstudio@gmail.com>
2017-10-20 11:32:12 -0400

    clean data

--- compare_methods.py
+++ compare_methods.py
@@ -44,22 +44,22 @@ def compare_methods(N):
     results = dict(max_cost_func=max_cost_func, N=N)
 
     # ###################### Dynamical programming ######################
-    # results["dp_max"] = QStochDynProg(**sys).make_rounds(20).max_cost() / max_cost_func
-    #
-    # # save number of calls made during optimization
-    # results["dp_calls"] = propagator.calls
-    #
-    # # reset the count
-    # propagator.calls = 0
-    #
-    # ################ Hybrid monte-carlo tree seach and dynamical programming #############
-    # results["mcdp_max"] = MTCDynProg(nsteps=10, **sys).make_rounds(2 ** 10).max_cost() / max_cost_func
-    #
-    # # save number of calls made during optimization
-    # results["mcdp_calls"] = propagator.calls
-    #
-    # # reset the count
-    # propagator.calls = 0
+    results["dp_max"] = QStochDynProg(**sys).make_rounds(20).max_cost() / max_cost_func
+
+    # save number of calls made during optimization
+    results["dp_calls"] = propagator.calls
+
+    # reset the count
+    propagator.calls = 0
+
+    ################ Hybrid monte-carlo tree seach and dynamical programming #############
+    results["mcdp_max"] = MTCDynProg(nsteps=10, **sys).make_rounds(2 ** 10).max_cost() / max_cost_func
+
+    # save number of calls made during optimization
+    results["mcdp_calls"] = propagator.calls
+
+    # reset the count
+    propagator.calls = 0
 
     ###################### Monte-Carlo tree search ######################
     results["mcts_max"] = MCTreeSearch(nsteps=2 ** 10, **sys).make_rounds(2 ** 9).max_cost() / max_cost_func
@@ -71,7 +71,7 @@ def compare_methods(N):
     propagator.calls = 0
 
     ###################### Genetic algorithm ######################
-    results["ga_max"] = GA(nsteps=2 ** 10, pop_size=2 ** 8, **sys).make_rounds(2 ** 8).max_cost() / max_cost_func
+    results["ga_max"] = GA(nsteps=2 ** 10, pop_size=2 ** 5, **sys).make_rounds(2 ** 6).max_cost() / max_cost_func
 
     # save number of calls made during optimization
     results["ga_calls"] = propagator.calls
@@ -82,7 +82,7 @@ def compare_methods(N):
     return results
 
 
-def plot_simulations():
+def plot_simulations(dir=''):
     """
     Plot results of simulations on a single plot
     """
@@ -93,15 +93,20 @@ def plot_simulations():
     def data2plot():
         """
         Generator yielding list to plot
-        :return:
         """
-        for fname in glob.glob("*.simul"):
+        for fname in glob.glob(dir + "*.simul"):
             with open(fname, "rb") as f:
                 yield pickle.load(f)
 
     for R in data2plot():
 
         N = map(itemgetter("N"), R)
+
+        dp_max = np.array(map(itemgetter("dp_max"), R))
+        dp_calls = np.array(map(itemgetter("dp_calls"), R))
+
+        mcdp_max = np.array(map(itemgetter("mcdp_max"), R))
+        mcdp_calls = np.array(map(itemgetter("mcdp_calls"), R))
 
         mcts_max = np.array(map(itemgetter("mcts_max"), R))
         mcts_calls = np.array(map(itemgetter("mcts_calls"), R))
@@ -115,12 +120,17 @@ def plot_simulations():
         plt.title("Normalized max values at the end of optimization")
         plt.plot(N, mcts_max, 'r*-', alpha=0.4, label="Monte-Carlo tree search")
         plt.plot(N, ga_max, 'b*-', alpha=0.4, label="Genetic algorithm")
+        plt.plot(N, mcdp_max, 'g*-', alpha=0.4, label="Monte-Carlo dynamical prog")
+        plt.plot(N, dp_max, 'y*-', alpha=0.4, label="Dynamical prog")
+
         #plt.legend()
 
         plt.subplot(122)
         plt.title("Algorithmic efficiency: Max-value per propagation")
         plt.plot(N, mcts_max / mcts_calls, 'r*-', alpha=0.4, label="Monte-Carlo tree search")
         plt.plot(N, ga_max / ga_calls, 'b*-', alpha=0.4, label="Genetic algorithm")
+        plt.plot(N, mcdp_max / mcdp_calls, 'g*-', alpha=0.4, label="Monte-Carlo dynamical prog")
+        plt.plot(N, dp_max / dp_calls, 'y*-', alpha=0.4, label="Dynamical prog")
         #plt.legend()
 
     plt.show()
@@ -136,7 +146,7 @@ if __name__=='__main__':
     #
     #############################################################################
 
-    p = Pool(10)
+    p = Pool(7)
     result = p.map(compare_methods, range(50, 3, -4))
 
     # save the results of simulations
